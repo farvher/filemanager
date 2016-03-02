3fe9bf28e759703ccb3204823572ba6cae270807
farvher <fesanmiguel@misena.edu.co>
2016-03-02 12:22:54 -0500

    sonar eclipse pom.xml

---
+++ src/main/java/com/farvher/filemanager/controller/BaseController.java
@@ -0,0 +1,23 @@
+package com.farvher.filemanager.controller;
+
+
+public class BaseController {
+	
+	public static final String CANTIDAD ="cantidad";
+	public static final String NAVEGADOR ="navegador";  
+	public static final String UBICADO ="ubicado";
+	public static final String ROOT = "root";
+	public static final String IMG ="img";
+	public static final String IMAGE ="image";
+	public static final String TIPO = "tipo" ;
+	public static final String INDEX = "index";
+	public static final String FILEMANAGER_PATH = "content/filemanager"; 
+	public static final String ERROR = "error";
+	public static final String ERROR404_PATH = "error/404";
+	public static final String CURRENT_DIRECTORY = System.getProperty("user.home");
+	
+	public BaseController() {
+		
+		
+	}
+}
---
+++ src/main/java/com/farvher/filemanager/controller/UsuarioController.java
@@ -0,0 +1,9 @@
+package com.farvher.filemanager.controller;
+
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class UsuarioController {
+
+}
